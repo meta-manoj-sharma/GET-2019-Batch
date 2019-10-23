f9b13a8965a5a3de6d5f00a1df1923f10936181a
meta-manoj-sharma <manoj.sharma@metacube.com>
2019-10-23 13:44:32 +0530

    Assignment of angular based on shopping app

---
+++ UI-Session-11/REST-API/src/dao/Query.java
@@ -0,0 +1,15 @@
+/*
+ * Query Class
+ */
+package dao;
+
+public class Query {
+	/**
+	 * Gets All items from table
+	 * @return query
+	 */
+	public static String getAll(){
+		String query = "select * from items ";
+		return query;
+	}
+}
